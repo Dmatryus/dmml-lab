3d6efd989b958a137621113b9d51aaadf8f609ad
Dmatryus <dmatryus.sqrt24@yandex.ru>
2022-09-03 13:08:51 +0300

    nn begin

--- dmml-lib/ml/clustering.py
+++ dmml-lib/ml/clustering.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from math.geometry import PointSet, Rangemeter
 
-
+# TODO: Rework for better usage: calculate + analysis in one class + minimal automl
 class Clustering(ABC):
     def __init__(self, analysis_mode=False):
         self.analysis_mode = analysis_mode
@@ -18,7 +18,7 @@ class Clustering(ABC):
         pass
 
 
-class SimplexBlackHole(Clustering):
+class SimplexBlackHoleClustering(Clustering):
     def update_range_function(self):
         if self.relative:
             self.range_function = lambda x, y: ((y - x) / x)
@@ -121,7 +121,7 @@ class DBSCAN(Clustering):
         return self.result
 
 
-class Hierarchical(Clustering):
+class HierarchicalClustering(Clustering):
     def init_meta(self, **kwargs):
         self._meta = skc.AgglomerativeClustering(
             n_clusters=self.n_clusters,
@@ -151,3 +151,310 @@ class Hierarchical(Clustering):
         ft = self._meta.fit(vectorized_data)
         self.result = ft.labels_
         return self.result
+
+class ClusteringAnalysis:
+    def __init__(
+        self,
+        object_: Clustering,
+        data=None,
+        data_2d=None,
+        plots_path=None,
+        plot_mark=None,
+        **kwargs,
+    ):
+        self.object_ = object_
+        self.data = data
+        self.data_2d = data_2d
+        self.plots_path = plots_path
+        self.plot_mark = plot_mark
+        self.metrics = {}
+
+    def data_to_2d(self, **kwargs):
+        mode = kwargs.get("mode_data_to_2d") or "umap"
+        if mode == "umap":
+            self.data_2d = umap.UMAP().fit_transform(self.data)
+
+    def result_map(self, **kwargs):
+        if self.data_2d is None:
+            self.data_to_2d(**kwargs)
+
+        scatter_map(
+            self.data_2d,
+            self.object_.result,
+            self.plots_path,
+            "Result 2d map",
+            self.plot_mark,
+        )
+
+    @staticmethod
+    def scatter_map(data_2d, values, plots_path=None, title=None, mark=None, **kwargs):
+        plt.figure(figsize=kwargs.get("figsize", (12, 10)))
+        plt.scatter(
+            data_2d[:, 0],
+            data_2d[:, 1],
+            c=values,
+            edgecolor="none",
+            alpha=kwargs.get("alpha", 0.4),
+            s=40,
+            cmap=plt.cm.get_cmap("nipy_spectral", len(np.unique(values))),
+        )
+
+        title = title + f" ({mark})" if mark else title
+
+        plt.title(title)
+        if plots_path and title:
+            plt.savefig(plots_path / f"{title}.jpg")
+        else:
+            plt.show()
+
+    def calculate_metric(self):
+        label_counts = sorted(
+            np.array(np.unique(self.object_.result, return_counts=True)).T,
+            key=lambda kv: kv[1],
+            reverse=True,
+        )
+        self.metrics["labels_count"] = len(np.unique(self.object_.result))
+        self.metrics["top_10_clusters"] = {
+            label: count for label, count in label_counts[:10]
+        }
+        X = self.data if self.data is not None else self.data_2d
+        if X is not None:
+            self.metrics["silhouette_score"] = metrics.silhouette_score(
+                X, self.object_.result
+            )
+            self.metrics["calinski_harabasz_score"] = metrics.calinski_harabasz_score(
+                X, self.object_.result
+            )
+            self.metrics["davies_bouldin_score"] = metrics.davies_bouldin_score(
+                X, self.object_.result
+            )
+
+    def analyze(self):
+        self.result_map()
+        self.calculate_metric()
+
+
+class DBSCANAnalysis(ClusteringAnalysis):
+    def outers_map(self):
+        if len(self.object_.outers_indexes) > 0:
+            outers_points = np.array(
+                [self.data_2d[i] for i in self.object_.outers_indexes]
+            )
+            scatter_map(
+                outers_points,
+                [-1 for i in outers_points],
+                self.plots_path,
+                "Outers Map",
+                self.plot_mark,
+            )
+
+    def clear_map(self):
+        if len(self.object_.outers_indexes) > 0:
+            points = np.array(
+                [
+                    self.data_2d[i]
+                    for i in range(len(self.data_2d))
+                    if i not in self.object_.outers_indexes
+                ]
+            )
+            scatter_map(
+                points,
+                [v for v in self.object_.result if v >= 0],
+                self.plots_path,
+                "Clear Map",
+                self.plot_mark,
+            )
+
+    def calculate_metric(self):
+        super().calculate_metric()
+        self.metrics["outrrs_count"] = len(self.object_.outers_indexes)
+        self.metrics["clean_clusters"] = 1 - len(self.object_.outers_indexes) / len(
+            self.object_.result
+        )
+
+    def analyze(self):
+        self.result_map()
+        self.outers_map()
+        self.clear_map()
+
+        self.calculate_metric()
+
+
+class SimplexBlackHoleAnalysis(ClusteringAnalysis):
+    def __init__(
+        self,
+        object_: Clustering,
+        data=None,
+        data_2d=None,
+        plots_path=None,
+        plot_mark=None,
+        **kwargs,
+    ):
+        super().__init__(object_, data, data_2d, plots_path, plot_mark, **kwargs)
+
+    def distances_plot(self, **kwargs):
+        plt.figure(figsize=kwargs.get("figsize", (12, 10)))
+        for d in self.object_.distances:
+            plt.plot(sorted(d))
+        plt.legend(range(1, len(self.object_.distances) + 1))
+        title = "Black Hole Distance"
+        title = title + f" ({self.plot_mark})" if self.plot_mark else title
+        plt.grid()
+        plt.title(title)
+        if self.plots_path and title:
+            plt.savefig(self.plots_path / f"{title}.jpg")
+        else:
+            plt.show()
+
+    def black_hole_ranges_plot(self, mode="full", **kwargs):
+        plt.figure(figsize=kwargs.get("figsize", (12, 10)))
+        if mode == "up":
+            x = []
+            y = []
+            for range_ in self.object_.ranges:
+                for i, r in enumerate(range_.values()):
+                    if r > self.object_.epsilon:
+                        x.append(r)
+                        y.append(i)
+            ranges = [
+                [x for x in r.values() if x > self.object_.epsilon]
+                for r in self.object_.ranges
+            ]
+            title = "Black Hole Ranges Over Epsilon"
+        elif mode == "down":
+            ranges = [
+                [x for x in r.values() if x < self.object_.epsilon]
+                for r in self.object_.ranges
+            ]
+            title = "Black Hole Ranges Under Epsilon"
+        else:
+            ranges = [r.values() for r in self.object_.ranges]
+            title = "Black Hole Ranges"
+        for r in ranges:
+            plt.plot(r)
+        plt.plot([self.object_.epsilon for i in range(len(ranges[0]))])
+        plt.legend(range(1, len(self.object_.ranges) + 1))
+        title = title + f" ({self.plot_mark})" if self.plot_mark else title
+        plt.title(title)
+        plt.grid()
+        if self.plots_path and title:
+            plt.savefig(self.plots_path / f"{title}.jpg")
+        else:
+            plt.show()
+
+    def cluster_range_plot(self, mode="full", **kwargs):
+        plt.figure(figsize=kwargs.get("figsize", (12, 10)))
+        if mode == "up":
+            ranges = [
+                x for x in self.object_.cluster_ranges if x > self.object_.epsilon
+            ]
+            title = "Cluster Ranges Over Epsilon"
+        elif mode == "down":
+            ranges = [
+                x for x in self.object_.cluster_ranges if x < self.object_.epsilon
+            ]
+            title = "Cluster Ranges Under Epsilon"
+        else:
+            ranges = self.object_.cluster_ranges
+            title = "Cluster Ranges"
+        plt.plot(ranges)
+        plt.plot([self.object_.epsilon for i in range(len(ranges))])
+        title = title + f" ({self.plot_mark})" if self.plot_mark else title
+        plt.title(title)
+        plt.grid()
+        if self.plots_path and title:
+            plt.savefig(self.plots_path / f"{title}.jpg")
+        else:
+            plt.show()
+
+    def black_hole_distance_maps(self):
+        for i, d in enumerate(self.object_.distances):
+            scatter_map(
+                self.data_2d,
+                d,
+                self.plots_path,
+                f"Black hole distances {i}",
+                self.plot_mark,
+            )
+        scatter_map(
+            self.data_2d,
+            [np.mean(point) for point in np.array(self.object_.distances).T],
+            self.plots_path,
+            f"Black hole distances mean",
+            self.plot_mark,
+        )
+
+    def clatreization_epoch_maps(self):
+        for i, e in enumerate(self.object_.epochs):
+            scatter_map(
+                self.data_2d,
+                e,
+                self.plots_path,
+                f"Clatreization epoch {i}",
+                self.plot_mark,
+            )
+
+    def range_maps(self):
+        for i, r in enumerate(self.object_.ranges):
+            scatter_map(
+                self.data_2d,
+                list(r.values()),
+                self.plots_path,
+                f"Ranges {i}",
+                self.plot_mark,
+            )
+        scatter_map(
+            self.data_2d,
+            [
+                np.sum(point)
+                for point in np.array([list(r.values()) for r in self.object_.ranges]).T
+            ],
+            self.plots_path,
+            f"Sum ranges",
+            self.plot_mark,
+        )
+
+    def analyze(self):
+        self.distances_plot()
+        self.black_hole_ranges_plot()
+        self.black_hole_ranges_plot("up")
+        self.black_hole_ranges_plot("down")
+
+        self.cluster_range_plot()
+        self.cluster_range_plot("up")
+        self.cluster_range_plot("down")
+
+        self.black_hole_distance_maps()
+
+        self.range_maps()
+
+        self.clatreization_epoch_maps()
+        super().analyze()
+
+
+class ClusteringAnalyser:
+    def __init__(self, report_path: str, plot_mark: str = None):
+        self.report_path = Path(report_path)
+        self._plots_path = self.report_path / "plots"
+        if not self._plots_path.is_dir():
+            self._plots_path.mkdir()
+
+        self.plot_mark = plot_mark
+
+    _analysis_mapping = {
+        DBSCAN: DBSCANAnalysis,
+        SimplexBlackHole: SimplexBlackHoleAnalysis,
+    }
+
+    def analyze(self, object_: Clustering, data=None, data_2d=None, **kwargs):
+        class_ = object_.__class__
+        analysis_class = (
+            self._analysis_mapping[class_]
+            if class_ in self._analysis_mapping
+            else ClusteringAnalysis
+        )
+        analysis = analysis_class(
+            object_, data, data_2d, self._plots_path, self.plot_mark, **kwargs
+        )
+        analysis.analyze()
+        return analysis
---
+++ dmml-lib/ml/nn.py
@@ -0,0 +1,66 @@
+class NN:
+    def __init__(self, model, optimazer, init_function=None):
+        self.model = model
+        self.optimazer = optimazer
+        self.init_function = init_function
+
+    def init_weights(self):
+        if self.init_function:
+            self.model.apply(self.init_function)
+        else:
+            # TODO: warning
+            pass
+
+
+class GanProcessor:
+    def __init__(self, generator, discriminator, len_latent_vector, device, dataloader):
+        self.generator = generator
+        self.discriminator = discriminator
+        self.len_latent_vector = len_latent_vector
+        self.device = device
+        self.dataloader = dataloader
+
+        self.img_list = []
+        self.generator_losses = []
+        self.discriminator_losses = []
+        self.iters = 0
+
+    def train(self, num_epoch):
+        def train_discriminator(data):
+            self.discriminator.model.zero_grad()
+            # Format batch
+            real_cpu = data[0].to(device)
+            b_size = real_cpu.size(0)
+            label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
+            # Forward pass real batch through D
+            output = self.discriminator(real_cpu).view(-1)
+            # Calculate loss on all-real batch
+            err_real = criterion(output, label)
+            # Calculate gradients for D in backward pass
+            err_real.backward()
+            x = output.mean().item()
+
+            ## Train with all-fake batch
+            # Generate batch of latent vectors
+            noise = torch.randn(b_size, self.len_latent_vector, 1, 1, device=device)
+            # Generate fake image batch with G
+            fake = self.discriminator.model(noise)
+            label.fill_(fake_label)
+            # Classify all fake batch with D
+            output = self.discriminator.model(fake.detach()).view(-1)
+            # Calculate D's loss on the all-fake batch
+            err_fake = criterion(output, label)
+            # Calculate the gradients for this batch, accumulated (summed) with previous gradients
+            err_fake.backward()
+            d_g_z1 = output.mean().item()
+            # Compute error of D as sum over the fake and the real batches
+            err = err_real + err_fake
+            # Update D
+            self.discriminator.optimazer.step()
+
+
+
+        print("Starting Training Loop...")
+        for epoch in range(num_epoch):
+            for i, data in enumerate(dataloader, 0):
+                train_discriminator(data)
